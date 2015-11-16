9cbf3a0e679ba4aedd4e9207e72ad4ec159088b1
Mihai Savin <savin.mihai@gmail.com>
2015-11-16 22:23:24 +0200

    Added all required methods to operate CarWash class in a skeleton format.

--- src/CarWash.java
+++ src/CarWash.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  */
@@ -13,7 +16,7 @@ public class CarWash {
 	 * Opens the CarWash for business.
 	 */
 	public void openShop() {
-		// TODO Auto-generated method stub
+		System.out.println("Shop OPENED.");
 		
 	}
 
@@ -26,4 +29,49 @@ public class CarWash {
 		
 	}
 
+	/**
+	 * Washes the first car in queue.
+	 */
+	public void washCar() {
+		System.out.println("Car WASHED.");		
+	}
+
+	
+	/**
+	 * Closes the CarWash for business.
+	 */
+	public void closeShop() {
+
+		System.out.println("Shop CLOSED.");
+		
+	}
+
+	
+	/**
+	 * Generates the List of cars that where washed today.
+	 * @return a List of Cars. 
+	 */
+	public List<Car> getTodaysClients() {
+
+		List<Car> theCarsList = new ArrayList<Car>();
+		theCarsList.add(new Logan(5, "ASSAAAVVV"));
+		theCarsList.add(new Logan(10, "WASSAAAVVV"));
+		theCarsList.add(new Logan(10, "XWASSAAAVVV"));
+		return theCarsList;		
+	}
+
+		
+	/**
+	 * Generates the List of cars that where not washed today.
+	 * @return a list of Cars.
+	 */
+	public List<Car> getPostponedClients() {
+		List<Car> theCarsList = new ArrayList<Car>();
+		theCarsList.add(new Logan(5, "ASSAAAVVV"));
+		theCarsList.add(new Logan(10, "WASSAAAVVV"));
+		theCarsList.add(new Logan(10, "XWASSAAAVVV"));
+		return theCarsList;		
+
+	}
+	
 }
