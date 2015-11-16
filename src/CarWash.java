1ca48947fd0d97eb0a47735a9701c8505fdb3e5f
Mihai Savin <savin.mihai@gmail.com>
2015-11-16 23:06:46 +0200

    Implemented reporting methods.

--- src/CarWash.java
+++ src/CarWash.java
@@ -6,12 +6,14 @@ import java.util.List;
  */
 
 /**
- * @author Meehai Savin 
- * Created @16.11.2015
+ * @author Meehai Savin Created @16.11.2015
  *
  */
 public class CarWash {
 	private boolean open;
+	private byte currentCar = 0;
+	private byte lastWashedCar = -1;
+	private ArrayList<Car> carsList = new ArrayList<Car>(); //Holds the Cars waiting to be washed.
 
 	/**
 	 * Opens the CarWash for business.
@@ -19,61 +21,66 @@ public class CarWash {
 	public void openShop() {
 		open = true;
 		System.out.println("Shop OPENED.");
-		
 	}
 
 	/**
 	 * Inserts a Car in the queue for washing.
+	 * 
 	 * @param theCar
 	 */
 	public void standInLine(Car aCar) {
-		// TODO Auto-generated method stub
-		
+		carsList.add(aCar);
+		System.out.println("Added the Car to the waiting list (washing queue).");
 	}
 
 	/**
-	 * Washes the first car in queue.
+	 * Washes the next car in queue.
 	 */
 	public void washCar() {
-		System.out.println("Car WASHED.");		
+		System.out.println("Car #" + currentCar++ + " WASHED.");
 	}
 
-	
 	/**
 	 * Closes the CarWash for business.
 	 */
 	public void closeShop() {
-
 		System.out.println("Shop CLOSED.");
-		
+		if (currentCar > 0) {
+			lastWashedCar = (byte) (currentCar - 1);
+		} else {
+			System.out.println("No Cars washed today.");
+		}
 	}
 
-	
 	/**
 	 * Generates the List of cars that where washed today.
-	 * @return a List of Cars. 
+	 * 
+	 * @return a List of Cars.
 	 */
 	public List<Car> getTodaysClients() {
-
-		List<Car> theCarsList = new ArrayList<Car>();
-		theCarsList.add(new Logan(5, "ASSAAAVVV"));
-		theCarsList.add(new Logan(10, "WASSAAAVVV"));
-		theCarsList.add(new Logan(10, "XWASSAAAVVV"));
-		return theCarsList;		
+		ArrayList<Car> theCarsList = new ArrayList<Car>();
+		if (currentCar > 0) {
+			for (int i = 0; i < currentCar; i++) {
+				theCarsList.add(carsList.get(i));
+			}
+		}
+		return theCarsList;
 	}
 
-		
 	/**
 	 * Generates the List of cars that where not washed today.
+	 * 
 	 * @return a list of Cars.
 	 */
 	public List<Car> getPostponedClients() {
-		List<Car> theCarsList = new ArrayList<Car>();
-		theCarsList.add(new Logan(5, "ASSAAAVVV"));
-		theCarsList.add(new Logan(10, "WASSAAAVVV"));
-		theCarsList.add(new Logan(10, "XWASSAAAVVV"));
-		return theCarsList;		
+		ArrayList<Car> theCarsList = new ArrayList<Car>();
+		if (currentCar > 0) {
+			for (int i = currentCar; i < carsList.size(); i++) {
+				theCarsList.add(carsList.get(i));
+			}
+		}
+		return theCarsList;
 
 	}
-	
+
 }
